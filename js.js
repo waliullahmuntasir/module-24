0c7e22218fd486768e86daaa8a1fcdf6e72e9e17
Waliullah Muntasir <waliullah.muntasir@gmail.com>
2025-01-21 12:42:44 +0600

    quary

--- js.js
+++ js.js
@@ -2,5 +2,10 @@
 // for (var suiii of list) {
 //     console.log(suiii.innerText);
 // }
-var iiii = document.getElementById('suii').innerText ;
+// var iiii = document.getElementById('suii').innerText ;
+// console.log(iiii)
+
+
+var iiii = document.querySelector('.suii')
+// var iiii = document.querySelectorAll('.suii')
 console.log(iiii)
