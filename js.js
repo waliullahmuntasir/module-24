90fe69c0c5ee842a2f7d8487875f4acdce2afd06
Waliullah Muntasir <waliullah.muntasir@gmail.com>
2025-01-21 12:28:54 +0600

    getelemntsbyid

--- js.js
+++ js.js
@@ -1,4 +1,6 @@
-var list = document.getElementsByTagName('p');
-for (var suiii of list) {
-    console.log(suiii.innerText);
-}
+// var list = document.getElementsByTagName('p');
+// for (var suiii of list) {
+//     console.log(suiii.innerText);
+// }
+var iiii = document.getElementById('suii').innerText ;
+console.log(iiii)
